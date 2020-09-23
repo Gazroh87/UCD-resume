5329bfb8f535026d8c0e9682610067fdca41deec
Gary Hughes <gary.hughes87@virginmedia.com>
2020-09-23 14:24:18 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -30,6 +30,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+  $("#gh-user-data").html("");
+  $("#gh-repo-data").html("");
   let username = $("#gh-username").val();
   if (!username) {
     $("#gh-user-data").html(`<h2>^ Please enter a GitHub username above</h2>`);
@@ -64,3 +66,5 @@ function fetchGitHubInformation(event) {
     }
   );
 }
+
+$(document).ready{fetchGitHubInformation};
